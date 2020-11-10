e2a826b9e37d46177ef67864bd9998760ae83449
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2020-11-10 13:04:41 -0800

    Completed Task 1

--- index.js
+++ index.js
@@ -4,17 +4,39 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
 
+ finals2014.forEach(function(item){
+    console.log(item["Home Team Name"]);
+});
+
 //(b) Away Team name for 2014 world cup final
+finals2014.forEach(function(item){
+    console.log(item["Away Team Name"]);
+});
 
 //(c) Home Team goals for 2014 world cup final
+finals2014.forEach(function(item){
+    console.log(item["Home Team Goals"]);
+});
 
 //(d) Away Team goals for 2014 world cup final
+finals2014.forEach(function(item){
+    console.log(item["Away Team Goals"]);
+});
 
 //(e) Winner of 2014 world cup final */
-
+finals2014.filter(function(item){
+    if (item["Home Team Goals"] > item["Away Team Goals"]){
+        console.log(`${item["Home Team Name"]} won the World Cup Final.`)
+    } else if (item["Home Team Goals"] < item["Away Team Goals"]){
+        console.log(`${item["Away Team Name"]} won the World Cup Final.`)
+};
+});
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
